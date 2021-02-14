196c89fb4dd227887a60c52f53d74017dc54eced
Omar Negron <omarnegron92@hotmail.com>
2021-02-13 20:25:40 -0500

    commit project

---
+++ src/main/java/Graph.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+// A Java program to print topological sorting of a DAG
+public class Graph {
+    // No. of vertices
+    private int V;
+
+    // Adjacency list as ArrayList of ArrayList's
+    private ArrayList<ArrayList<Integer>> adj;
+}
